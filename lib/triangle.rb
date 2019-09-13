e18c60731fa4a0f39bdd5bf892adfd5d46666a57
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-13 14:23:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c
+    #binding.pry 
+  end 
+
+  
+  def kind
+    if @a && @b && @c == @a && @b && @c
+      :equilateral
+    elsif @a && @c == @a && @c || @a && @b == @a && @b || @b && @c == @b && @c 
+      :isosceles
+    elsif @a && @b && @c != @a && @b && @c
+      :scalene
+    elsif ((@a == 0 && @b == 0 && @c == 0) || (@a < 0 || @b < 0 || @c < 0) || (@a + @b < @c || @b + @c < @a || @c + @b < @a))
+      begin
+        raise TriangleError
+      end 
+    end
+    #binding.pry 
+  end 
+  
+  class TriangleError < StandardError
+  end
 end
